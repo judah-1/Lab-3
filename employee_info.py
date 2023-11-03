1955c72bb0731be959d9f2072b4c38e03014c341
Judah <judah.22@ichat.sp.edu.sg>
2023-11-03 18:29:30 +0800

    Coded employees by department

--- employee_info.py
+++ employee_info.py
@@ -32,9 +32,10 @@ def calculate_average_salary():
 def get_employees_by_dept(department):
     result = []
 
+    for item in employee_data:
+        if item["department"] == department:
+            result.append(item)
     # Add your implementation from here
-
-
     return result
 
 def display_all_records():
